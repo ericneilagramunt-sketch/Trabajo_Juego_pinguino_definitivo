8c2495087b13bd08da9a30fac50dd6914eaf0cd6
jorgeclarosala-hash <jorgeclarosala@alumnes.ilerna.com>
2026-03-08 22:30:47 +0100

    Update Tablero.java
    Esta clase genera el tablero de forma aleatoria. Al crearlo, rellena cada posición con un tipo de casilla elegido al azar entre cinco opciones (oso, agujero, trineo, evento o suelo quebradizo). También tiene un método para activar el efecto de la casilla donde se encuentra un jugador en cada momento.

--- Tablero.java
+++ Tablero.java
@@ -1,1 +1,28 @@
+package reto;
+import java.util.*;
+import java.util.Random;
+import java.util.ArrayList;
+public class tablero {
+  private ArrayList<Casilla> casillas;
+  public tablero(int size) {
+      casillas = new ArrayList<>();
+      Random r = new Random();
+      for (int i = 0; i < size; i++) {
+          int tipo = r.nextInt(5);
+          switch (tipo) {
+              case 0: casillas.add(new oso(i)); break;
+              case 1: casillas.add(new agujero(i)); break;
+              case 2: casillas.add(new trineo(i)); break;
+              case 3: casillas.add(new evento(i)); break;
+              default: casillas.add(new suelo_quebradizo(i)); break;
+          }
+      }
+  }
+  public void activarCasilla(jugador j) {
+      casillas.get(j.getPosicion()).realizarAccion(j);
+  }
+  public int getSize() {
+      return casillas.size();
+  }
+}
 
