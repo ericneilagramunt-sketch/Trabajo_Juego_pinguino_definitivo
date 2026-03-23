10028bc096ca7c0c3bf9df7d0da3b93450d90885
ericneilagramunt-sketch <ericneilagramunt@alumnes.ilerna.com>
2026-03-23 16:27:59 +0100

    Creo el método del uso de la bola de nieve y sus efectos

--- src/reto/BoladeNieve.java
+++ src/reto/BoladeNieve.java
@@ -3,3 +3,17 @@ public class BolaDeNieve extends Item {
     public BolaDeNieve(String nombre, int cantidad) {
         super(nombre, cantidad);
     }
+ public void usar(Jugador objetivo) {
+        if (getCantidad() > 0) {
+            System.out.println("¡Bola de nieve lanzada a " + objetivo.getNombre() + "!");
+
+            // Ejemplo de efecto: el jugador pierde turno
+            objetivo.setPierdeTurno(true);
+
+            // Reducimos la cantidad
+            setCantidad(getCantidad() - 1);
+        } else {
+            System.out.println("No quedan bolas de nieve.");
+        }
+    }
+}
