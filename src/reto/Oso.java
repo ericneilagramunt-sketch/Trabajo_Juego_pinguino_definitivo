45bb06d499198a465744d91651c685fb5a7295d8
ericneilagramunt-sketch <ericneilagramunt@alumnes.ilerna.com>
2026-03-23 16:21:14 +0100

    Creo el metodo de realizarAccion

--- src/reto/Oso.java
+++ src/reto/Oso.java
@@ -3,3 +3,15 @@ public class Oso extends Casilla {
     public Oso(int posicion) {
         super(posicion);
     }
+  @Override
+    public void realizarAccion(Partida partida, Jugador jugador) {
+        System.out.println("¡Un oso ataca!");
+
+        int nuevaPos = jugador.getPosicion() - 1;
+        if (nuevaPos < 0) nuevaPos = 0;
+
+        jugador.setPosicion(nuevaPos);
+
+        System.out.println("El jugador retrocede a la posición " + nuevaPos);
+    }
+}
